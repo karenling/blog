2303a14c0d0114ead311c31c7bba3c969d134bcd
Karen Ling <karensling@gmail.com>
2016-04-22 02:23:55 -0700

    Add user resource.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,17 @@ class User < ActiveRecord::Base
   # allow password to be blank for situations where we don't
   # plan on changing/setting the password
   validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
 
   def password=(password)
     @password = password
@@ -14,5 +25,17 @@ class User < ActiveRecord::Base
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 end
