f96c644932beb22c827283a3a811c10acf43793f
Karen Ling <karensling@gmail.com>
2016-04-22 04:27:06 -0700

    Start specs for user.

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,61 @@
+module RspecLoginHelper
+  def login_as(user)
+    request.session[:session_token] = user.session_token
+    user
+  end
+
+  def current_user
+    User.find(request.session[:session_token])
+  end
+end
+include RspecLoginHelper
+
+describe UsersController do
+  before :each do
+    @user = FactoryGirl.create(:user)
+  end
+
+  describe 'create' do
+    it 'should create a new user' do
+      params = {
+        user: {
+          email: Faker::Internet.email,
+          password: Faker::Internet.password(8)
+          }
+      }
+      expect{ post :create, params }.to change{ User.count }.by(1)
+      expect(assigns(:current_user).email).to eq(params[:user][:email])
+      expect(flash[:notice] = 'Account created!')
+    end
+    it 'should create a new user' do
+      params = {
+        user: {
+          email: Faker::Internet.email,
+          password: Faker::Internet.password(3)
+          }
+      }
+      expect{ post :create, params }.to change{ User.count }.by(0)
+      expect(flash[:error]).to eq('Password is too short (minimum is 6 characters)')
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'new' do
+    it 'should render new view' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'show' do
+    it 'should render show view for logged in user' do
+      login_as(@user)
+      get :show, id: @user.id
+      expect(response).to render_template(:show)
+    end
+    it 'should not render show view for non-logged in user' do
+      get :show, id: @user.id
+      expect(response).to redirect_to(new_session_path)
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe User do
+  before :each do
+    @user = FactoryGirl.create(:user, password: 'fakepassword')
+  end
+  
+  describe 'self.find_by_credentials' do
+    it 'should return correct user' do
+      returned_user = User.find_by_credentials(@user.email, @user.password)
+      expect(returned_user).to eq(@user)
+    end
+    it 'should return nil if user password doesn\'t match' do
+      returned_user = User.find_by_credentials(@user.email, 'pink')
+      expect(returned_user).to be(nil)
+    end
+  end
+
+  describe 'is_password?' do
+    it 'should return true if passwords match' do
+      expect(@user.is_password?('fakepassword')).to be(true)
+    end
+    it 'should return false if passwords don\'t match' do
+      expect(@user.is_password?('fakepasswrd')).to be(false)
+    end
+  end
+
+  describe 'reset_session_token!' do
+    it 'should reset user session token' do
+      original_session_token = @user.session_token
+      @user.reset_session_token!
+      expect(@user.session_token).to_not eq(original_session_token)
+    end
+  end
+
+  describe 'ensure_session_token' do
+    it 'should generate a session token for user after initialize' do
+      expect(User.new.session_token).to_not be("")
+    end
+  end
+end
