20ea7a99b5c9a3973da61d83c33b68fa3c3b1cd7
Karen Ling <karensling@gmail.com>
2016-04-29 14:23:59 -0700

    Redirect.

--- config/routes.rb
+++ config/routes.rb
@@ -64,4 +64,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get '*path' => redirect('/') #if Rails.env.production?
 end
