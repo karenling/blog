a1bf8b7f979a02d829bb1f5178f141f1601cba2d
Karen Ling <karensling@gmail.com>
2016-04-30 06:57:17 -0700

    Production differences.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+abort if Rails.env.production?
+
 user = FactoryGirl.create(:user, email: 'test@test.com', password: 'testing')
 2.times do
   FactoryGirl.create(:post, author: user, status: Post::PUBLIC)
--- spec/factories.rb
+++ spec/factories.rb
@@ -9,7 +9,7 @@ FactoryGirl.define do
     title { Faker::Book.title }
     body { Faker::Lorem.paragraph(4) }
     status { [Post::PRIVATE, Post::DRAFT, Post::PUBLIC].sample }
-    post_date { Time.current }
+    post_date { Time.current + rand(-50..50).days }
     tag_list { "#{Faker::Color.color_name}, #{Faker::Color.color_name}, #{Faker::Color.color_name}" }
   end
 end
