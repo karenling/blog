2231fa435b7b3d9e81a36833e701d7927e087059
Karen Ling <karensling@gmail.com>
2016-06-07 21:14:51 -0700

    Spec for set_friendly_name!

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 describe Post do
   describe 'humanized_post_date' do
     it 'should return properly formatted date' do
-      post = FactoryGirl.create(:post, post_date: 'November 28, 1989')
-      expect(post.humanized_post_date).to eq('Monday, November 27, 1989')
+      post = FactoryGirl.create(:post, post_date: Time.parse('Nov 27, 2016, 9:00am PST'))
+      expect(post.humanized_post_date).to eq('Sunday, November 27, 2016')
     end
   end
   describe 'humanized_status' do
@@ -17,6 +17,14 @@ describe Post do
     specify 'when public post' do
       post = FactoryGirl.create(:post, status: Post::PUBLIC)
       expect(post.humanized_status).to eq('Public')
+    end
+  end
+  describe 'set_friendly_name!' do
+    it 'should change friendly id' do
+      post = FactoryGirl.create(:post, post_date: Time.parse('Nov 18, 2016, 9:00am PST'))
+      post.update(title: 'Twinkie is awesome')
+      post.set_friendly_name!
+      expect(post.slug).to eq('2016-11-18-twinkie-is-awesome')
     end
   end
 end
