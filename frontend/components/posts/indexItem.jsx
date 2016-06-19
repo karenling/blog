c6722ecd6252e7df77eb40b8c92e394e08ba59ac
Karen Ling <karensling@gmail.com>
2016-06-18 22:17:21 -0700

    Update toggle edit.

--- frontend/components/posts/indexItem.jsx
+++ frontend/components/posts/indexItem.jsx
@@ -33,24 +33,20 @@ var PostIndexItem = React.createClass({
       moreButton = <div className='post-more'><Link to={ postLink }>Read More</Link></div>
     }
 
+    var view;
     if (this.state.showEdit) {
       view = <PostEdit key={ this.props.post.friendly_name + 'edit' } post={ this.props.post } toggleView={ this.toggleView }/>
-    } else {
-      view = (
-        <div>
-          <div className='post-title'><Link to={ postLink }>{ this.props.post.title }</Link></div>
-          <div className='post-detail'>
-            <em>By</em> Karen <em>on</em> { this.props.post.post_date }
-            { status } {editLink }
-          </div>
-          <div className='post-body' dangerouslySetInnerHTML={ this.createMarkup() } />
-          { moreButton }
-        </div>
-      )
     }
+    
     return(
       <article>
-
+        <div className='post-title'><Link to={ postLink }>{ this.props.post.title }</Link></div>
+        <div className='post-detail'>
+          <em>By</em> Karen <em>on</em> { this.props.post.post_date }
+          { status } {editLink }
+        </div>
+        <div className='post-body' dangerouslySetInnerHTML={ this.createMarkup() } />
+        { moreButton }
         { view }
       </article>
     )
