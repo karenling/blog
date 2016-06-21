fcc4e23e7beaa48edea6791af4fca6987d396a5f
Karen Ling <karensling@gmail.com>
2016-06-20 23:28:33 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,13 @@ module.exports = {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ]
   resolve: {
     extensions: ["", ".js", ".jsx"]
   },
