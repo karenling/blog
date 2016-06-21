2eb6c0c9a8440b0110cce9267a71a68db7edb273
Karen Ling <karensling@gmail.com>
2016-06-20 23:17:36 -0700

    put production in normal web pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
-var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
   entry: "./frontend/twinkieandkaren.jsx",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    path: "./app/assets/javascripts",
     filename: "bundle.js"
   },
-  resolve: {
-    extensions: ["", ".js", ".jsx"]
-  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['react']
         }
-      },
-      {
-        test: /\.node$/,
-        loader: "node-loader"
       }
     ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
   }
 };
