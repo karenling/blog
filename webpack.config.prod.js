ab74d7c5e9d63620a669c67dad7b38ab42629776
Karen Ling <karensling@gmail.com>
2016-06-20 22:46:26 -0700

    Production

---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/twinkieandkaren.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  }
+};
