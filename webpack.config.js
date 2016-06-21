52756486158187bf0426d9219e5fcb5eca33e855
Karen Ling <karensling@gmail.com>
2016-06-20 23:25:49 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,29 @@
-var webpack = require("webpack");
+var path = require("path");
 
 module.exports = {
   context: __dirname,
   entry: "./frontend/twinkieandkaren.jsx",
   output: {
-    path: "./app/assets/javascripts",
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
-  plugins:[
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
-      }
-    })
-  ],
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
           presets: ['react']
         }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
       }
     ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ["", ".js", ".jsx"]
   }
 };
