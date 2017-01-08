bec865df1bdd6b09dcc356cb61031b9e18724cd9
Karen Ling <karensling@gmail.com>
2017-01-08 01:38:13 -0800

    More root.

--- frontend/components/Root.jsx
+++ frontend/components/Root.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'redux-little-router';
 
 import Contact from './Contact';
 
-const Root = props => (
+import { toggleContactModal } from '../actions';
+
+const _Root = props => (
   <div>
     <nav className="navigationBar">
       <Link href="/posts" className="navigationBar--link">Posts</Link>
       <Link href="/about" className="navigationBar--link">About</Link>
+      <a onClick={props.toggleContactModal} className="navigationBar--link">Contact</a>
     </nav>
     {props.children}
     <Contact />
   </div>
 );
 
-Root.propTypes = {
+_Root.propTypes = {
   children: React.PropTypes.oneOfType([
     React.PropTypes.arrayOf(React.PropTypes.node),
     React.PropTypes.node,
   ]),
+  toggleContactModal: React.PropTypes.func.isRequired,
 };
+
+const Root = connect(
+  state => ({
+    router: state.router,
+  }),
+  { toggleContactModal },
+)(_Root);
 
 export default Root;
